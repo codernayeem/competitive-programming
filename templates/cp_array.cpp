27834ecea6f8ca2a265d92b979c48c96f4b9ce0d
Md Nayeem <codernayeem898@gmail.com>
2022-09-09 22:44:39 +0600

    added cp_array.cpp to start cp with array

---
+++ templates/cp_array.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, arr[n], i;
+    cin >> n;
+    for (i = 0; i < n; i++) cin >> arr[i];
+
+    // your tasks
+    for (i = 0; i < n; i++) cout << arr[i] << ' ';
+    cout << n << endl;
+}
