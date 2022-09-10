935ccf1a934de65e3c74211b1d30ebda4be02351
Md Nayeem <codernayeem898@gmail.com>
2022-09-10 22:51:36 +0600

    added insertion sort

---
+++ programs/insertion_sort.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+template <class T>
+void insertionSort(T arr[], int n){
+    int i, j, temp_i;
+    for (i = 1; i < n; i++){
+        temp_i = i;
+        j = i - 1;
+        while(arr[j] > arr[temp_i] && j >= 0){
+            swap(arr[j], arr[temp_i]);
+            j--;
+            temp_i--;
+        }
+    }
+}
+
+int main()
+{
+    int n, arr[n], i;
+    cin >> n;
+    for (i = 0; i < n; i++) cin >> arr[i];
+
+    insertionSort(arr, n);
+
+    for (i = 0; i < n; i++) cout << arr[i] << ' ';
+    cout << endl;
+}
