8872780837839a354aef0a1f11c26aa80d424968
Md Nayeem <codernayeem898@gmail.com>
2022-09-18 20:10:55 +0600

    added cp_test_array.cpp

---
+++ templates/cp_test_array.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t, n;
+    cin >> t;
+
+    for (int i = 0; i < t; i++){
+        cin >> n;
+
+        int arr[n];
+        for (int j = 0; j < n; j++) cin >> arr[j];
+        
+    }
+}
